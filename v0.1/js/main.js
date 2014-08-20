c4331ee0eb59d1ea2fd7012defe066ca4f7d5802
Zero <zeronbo@163.com>
2014-08-20 20:44:21 +0800

    初始化

---
+++ v0.1/js/main.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Leo on 2014/8/20.
+ */
+
+require(['window', function (w) {
+    new w.Window().alert();
+}]);
---
+++ v0.1/js/window.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Leo on 2014/8/20.
+ */
+
+define(function () {
+    function Window() {
+    }
+
+    Window.prototype = {
+        alert: function () {
+
+        },
+        confirm: function () {
+
+        },
+        prompt: function () {
+
+        }
+    };
+
+    return {
+        Window: Window
+    };
+
+});
