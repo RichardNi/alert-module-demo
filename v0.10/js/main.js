b819c26f218b6971eb9c42df62d6b8892b8b4916
Richrad <richardni@163.com>
2014-08-24 18:36:51 +0800

    弹窗组件v0.10版本
    - 增加模态弹窗功能；

---
+++ v0.10/js/main.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Richard on 2014/8/20.
+ */
+
+require.config({
+    paths: {
+        jquery: 'jquery-1.11.1.min'
+    }
+});
+
+require(['jquery', 'window'], function ($, w) {
+    $('#a').click(function () {
+        new w.Window().alert({
+            title: "消息",
+            content: "Welcome",
+            handlerAlertBtn: function () {
+                alert("你点击了Alert按钮");
+            },
+            width: 400,
+            height: 200,
+            y:200,
+            hasCloseBtn: true,
+            handerCloseBtn: function () {
+                alert("你点击了关闭按钮");
+            },
+            textAlertBtn: "OK"
+        });
+    });
+
+});
